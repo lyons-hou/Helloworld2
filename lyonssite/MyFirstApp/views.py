3738ed7b6553fd98720f69c63a4b86710b377516
Lyons.Hou <Lyons.Hou@Adlinktech.com>
2022-09-16 10:28:37 +0800

    init

---
+++ lyonssite/MyFirstApp/views.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello_world(request):
+    return render(request, 'hello_world.html',{
+        'current_time': str(datetime.now()),
+        })
+
